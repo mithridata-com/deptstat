ee695308aaafa3aec28e14d594ed72fec2fcde17
Dmitrii <35778260+mithridata-com@users.noreply.github.com>
2021-10-24 21:51:58 +0300

    New func added v.0.1.1
    New func added - read_and_bind_xlsx

---
+++ R/read_and_bind_xlsx.R
@@ -0,0 +1,37 @@
+#' read_and_bind_xlsx (package "deptstat")
+#'
+#' This function is created to bind similar files into one data frame.
+#'
+#' @param path A path to XLSX files
+#' @param filename_pattern A substring for subsetting files (part of file name)
+#' @param return_filenames A parameter, that can be set to TRUE if the output must contain source filenames. Default: FALSE
+#'
+#' @return A vector or data frame with outliers marked as 1 and normal observations marked as 0
+#'
+#' @examples
+#' # This will return a dataframe, based on binded XLSX files, which name contained 'data' as a substring
+#' read_and_bind_xlsx(getwd(), "data")
+#'
+#' #'  @author Dmitrii Diachkov (2021)
+#'
+
+#' @export
+read_and_bind_xlsx <- function(path, filename_pattern = "", return_filenames = F)
+{
+
+InPath <- paste0(file.path(path),"/")
+search_files <- str_detect(list.files(InPath), filename_pattern)
+
+d <- data.frame()
+for (xls_file in list.files(InPath)[search_files])
+{
+  print(xls_file)
+  file <- read_excel(paste0(InPath,xls_file), sheet = 1)
+  if(return_filenames == T)
+  {
+    file$FILENAME <- xls_file
+  }
+  d <- rbind(d, file)
+}
+return(d)
+}
